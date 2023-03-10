5218e48826ea7ef6a4f5f905969ac5ea56fba564
Robson <53387930+HunterLowBit@users.noreply.github.com>
2023-03-10 01:09:33 -0300

    alterações no posicionamento e clones de caixa de dialogos e labels

--- Aula_4.2/TKWidget.py
+++ Aula_4.2/TKWidget.py
@@ -6,8 +6,6 @@ class Application:
     def __init__(self, master=None):
         pass
 
-
-
 root = tk.Tk()
 root.geometry("300x200")
 
@@ -17,8 +15,6 @@ rotulo.pack()
 rotulo2 = tk.Label(root, text="Digite seu nome e selecione se deseja receber emails!")
 rotulo2.pack() 
 
-
-
 caixa_texto = tk.Entry(root,)
 caixa_texto.pack()  
 
@@ -26,14 +22,18 @@ opcoes = ["Sim", "Não"]
 menu = tk.OptionMenu(root, tk.StringVar(), *opcoes)
 menu.pack()  
 
+rotulo3 = tk.Label(root, text="Digite seu email caso tenha marcado que sim")
+rotulo3.pack() 
 
-caixa_selecao = tk.Checkbutton(root, text="Aceito os termos e condições")
-caixa_selecao.pack()  
+caixa_texto1 = tk.Entry(root,)
+caixa_texto1.pack()
 
+c_selecao = tk.Checkbutton(root, text="Aceito os termos e condições")
+c_selecao.pack()  
 
-btn = Button(root, text="Fechar", command=root.destroy)
+btn = Button(root, text="Confirmar e Fechar", command=root.destroy)
 btn.pack(pady=10)
-btn.place(x = 120, y = 150)
+btn.place(x = 90, y = 150)
 
 Application(root)
 root.mainloop()
