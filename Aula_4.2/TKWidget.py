3dc6680c45cd61062f8f275315433e2cffa4d9e0
Robson <53387930+HunterLowBit@users.noreply.github.com>
2023-03-10 01:05:04 -0300

    Widgets reajustados

--- Aula_4.2/TKWidget.py
+++ Aula_4.2/TKWidget.py
@@ -5,16 +5,35 @@ import tkinter as tk
 class Application:
     def __init__(self, master=None):
         pass
-root = Tk()
-root.geometry('300x300')
 
 
 
+root = tk.Tk()
+root.geometry("300x200")
+
+
+rotulo = tk.Label(root, text="Termos de Serviço!")
+rotulo.pack() 
+rotulo2 = tk.Label(root, text="Digite seu nome e selecione se deseja receber emails!")
+rotulo2.pack() 
 
 
 
-Botao_Sair = Button(root, text="Fechar", command=root.destroy)
-Botao_Sair.pack(pady=10)
-Botao_Sair.place(x = 120, y = 250)
+caixa_texto = tk.Entry(root,)
+caixa_texto.pack()  
+
+opcoes = ["Sim", "Não"]
+menu = tk.OptionMenu(root, tk.StringVar(), *opcoes)
+menu.pack()  
+
+
+caixa_selecao = tk.Checkbutton(root, text="Aceito os termos e condições")
+caixa_selecao.pack()  
+
+
+btn = Button(root, text="Fechar", command=root.destroy)
+btn.pack(pady=10)
+btn.place(x = 120, y = 150)
+
 Application(root)
 root.mainloop()
