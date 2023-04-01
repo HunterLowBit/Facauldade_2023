054ad28299b86de8c8880dfd8e9e1b552fd48f81
Robson <53387930+HunterLowBit@users.noreply.github.com>
2023-04-01 18:44:16 -0300

    Update loginV2.py

--- Interface_TK/Aula_6.1-login_tk/loginV2.py
+++ Interface_TK/Aula_6.1-login_tk/loginV2.py
@@ -21,7 +21,7 @@ janela.config(background=c1)
 janela.resizable(width=FALSE, height=FALSE) 
 
 
-frame_login = Frame(janela, bg=c0, height=190, width=290, relief='flat')
+frame_login = Frame(janela, bg=c0, height=190, width=290, relief='ridge')
 frame_login.grid(row=0, column=0, padx=5, pady=5,sticky="NSEW") 
 
 separador = Frame(janela, bg=c6, height=30, width=2, relief='flat')
@@ -36,10 +36,15 @@ def login():
         messagebox.showerror("Erro", "usuário ou senha incorretos") 
 
 
+
+label_linha = Label(separador,width=21, text="DIGITE AS CREDENCIAIS",anchor=NE, font=("Arial", 12), bg=c1 , fg=c0).place(x=12, y=12)
+
 Label1 = Label(separador, width=7, text="Usuário:", background=c6, foreground=c1, font=("Arial", 15, "bold")).place(x=8, y=50)
 Label2 = Label(separador, width=7, text="Senha:", background=c6, foreground=c1, font=("Arial", 15, "bold")).place(x=8, y=90)
 
- 
+#Fundo da entry
+label_linha = Label(separador,width=12, text="",anchor=NE, font=("Arial", 17, "bold"), bg=c1 , fg=c1).place(x=103, y=48)
+label_linha = Label(separador,width=12, text="",anchor=NE, font=("Arial", 17, "bold"), bg=c1 , fg=c1).place(x=103, y=88)
  
 usuario = Entry(separador, width=15, font=("Arial", 15, "bold"))
 usuario.place(x=105, y=50)   
@@ -47,10 +52,11 @@ usuario.place(x=105, y=50)
 senha = Entry(separador, width=15, show="*", font=("Arial", 15, "bold"))
 senha.place(x=105, y=90)
 
+
+#Botão de login estilizado
 label_linha = Label(separador,width=200, text="",anchor=NE, font=("Arial", 2, "bold"), bg=c1 , fg=c1).place(x=0, y=132)
 label_linha = Label(separador,width=200, text="",anchor=NE, font=("Arial", 2, "bold"), bg=c1 , fg=c1).place(x=0, y=164)
 label_linha = Label(separador,width=100, text="",anchor=NE, font=("Arial", 12, "bold"), bg=c5 , fg=c1).place(x=0, y=140)
-
 
 Button(separador, text="Entrar", command=login, background=c1, foreground=c0, font=("Arial", 15, "bold")).place(x=140, y=132)
 
