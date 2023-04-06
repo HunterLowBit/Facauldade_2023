c074a5475caf295ece9e026c7be3a28c43d1fa57
Robson <53387930+HunterLowBit@users.noreply.github.com>
2023-04-06 14:22:55 -0300

    Update loginV2 Refatore copy.py

--- Interface_TK/Aula_6.1-login_tk/p1_previa/loginV2 Refatore copy.py
+++ Interface_TK/Aula_6.1-login_tk/p1_previa/loginV2 Refatore copy.py
@@ -10,13 +10,19 @@ frame = Frame(root, bg="black",height=200, width=300)
 frame.grid(row=0, column=0)
 
 def login():
-    if (usuario.get()== 'robson' and senha.get() == '2402'):
+    if (usuario.get() == 'robson' and senha.get() == '2402'):
         messagebox.showinfo('bem vindo','sucesso')
         root.destroy()
     else:
         messagebox.showinfo('Errado','Tente novamente')
 
-label1 = Label(root, width=7, text='usuario',).place(x=8,y=50)
+label1 = Label(root, width=7, text='usuario',font=('Arial',15)).place(x=8,y=50)
+label1 = Label(root, width=7, text='senha',font=('Arial',15)).place(x=8,y=90)
 
+usuario = Entry(root,width=15,font=('Arial',15))
+usuario.place(x=110,y=50)
+senha = Entry(root, width=15,show='#',font=('Arial',15))
+senha.place(x=110,y=90)
+
+Button(root, text="login",font=('Arial',12),bg="white",command=login).place(x=150,y=150)
 root.mainloop()
-
