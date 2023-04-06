f4bfb946ffb1b7b300eb14601f0d6ac4c1142b8f
Robson <53387930+HunterLowBit@users.noreply.github.com>
2023-04-06 11:16:54 -0300

    dedicado a remover todo codigo extra e manter funcionando

---
+++ Interface_TK/Aula_6.1-login_tk/p1_previa/loginV2 Refatore.py
@@ -0,0 +1,28 @@
+from tkinter import *
+from tkinter import messagebox
+     
+janela = Tk()
+janela.title("Sistema de login")
+janela.geometry("300x200")
+janela.resizable(width=FALSE, height=FALSE) 
+
+frame_login = Frame(janela, bg='black', height=190, width=290, relief='ridge')
+frame_login.grid(row=0, column=0, padx=5, pady=5,sticky="NSEW") 
+
+def login():
+    if (usuario.get() == "Robson" and senha.get() == "2402"):
+        messagebox.showinfo('Sucesso', 'Seja bem vindo!') 
+        janela.destroy()
+    else:
+        messagebox.showerror("Erro", "usuário ou senha incorretos") 
+        
+Label1 = Label(frame_login, width=7, text="Usuário:", background='orange', foreground='black', font=("Arial", 15, "bold")).place(x=8, y=50)
+Label2 = Label(frame_login, width=7, text="Senha:", background='orange', foreground='black', font=("Arial", 15, "bold")).place(x=8, y=90)
+
+usuario = Entry(frame_login, width=15, font=("Arial", 15, "bold"))
+usuario.place(x=105, y=50)   
+senha = Entry(frame_login, width=15, show="*", font=("Arial", 15, "bold"),fg='black')
+senha.place(x=105, y=90)
+
+Button(frame_login, text="Entrar", command=login, background='orange', foreground='white', font=("Arial", 15, "bold")).place(x=140, y=132)
+janela.mainloop()
