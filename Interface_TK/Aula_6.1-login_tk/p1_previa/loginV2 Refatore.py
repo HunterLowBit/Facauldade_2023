735bb5c382f1aaa30f2390f01faba44dbb08f5a1
Robson <53387930+HunterLowBit@users.noreply.github.com>
2023-04-06 11:23:37 -0300

    Update loginV2 Refatore.py

--- Interface_TK/Aula_6.1-login_tk/p1_previa/loginV2 Refatore.py
+++ Interface_TK/Aula_6.1-login_tk/p1_previa/loginV2 Refatore.py
@@ -6,8 +6,8 @@ janela.title("Sistema de login")
 janela.geometry("300x200")
 janela.resizable(width=FALSE, height=FALSE) 
 
-frame_login = Frame(janela, bg='black', height=190, width=290, relief='ridge')
-frame_login.grid(row=0, column=0, padx=5, pady=5,sticky="NSEW") 
+frame_login = Frame(janela, bg='black', height=200, width=300, relief='ridge')
+frame_login.grid(row=0, column=0,) 
 
 def login():
     if (usuario.get() == "Robson" and senha.get() == "2402"):
@@ -16,13 +16,13 @@ def login():
     else:
         messagebox.showerror("Erro", "usuário ou senha incorretos") 
         
-Label1 = Label(frame_login, width=7, text="Usuário:", background='orange', foreground='black', font=("Arial", 15, "bold")).place(x=8, y=50)
-Label2 = Label(frame_login, width=7, text="Senha:", background='orange', foreground='black', font=("Arial", 15, "bold")).place(x=8, y=90)
+Label1 = Label(frame_login, width=7, text="Usuário:", background='orange', foreground='black', font=("Arial", 15, "bold")).place(x=10, y=50)
+Label2 = Label(frame_login, width=7, text="Senha:", background='orange', foreground='black', font=("Arial", 15, "bold")).place(x=10, y=90)
 
 usuario = Entry(frame_login, width=15, font=("Arial", 15, "bold"))
-usuario.place(x=105, y=50)   
+usuario.place(x=110, y=50)   
 senha = Entry(frame_login, width=15, show="*", font=("Arial", 15, "bold"),fg='black')
-senha.place(x=105, y=90)
+senha.place(x=110, y=90)
 
-Button(frame_login, text="Entrar", command=login, background='orange', foreground='white', font=("Arial", 15, "bold")).place(x=140, y=132)
+Button(frame_login, text="Entrar", command=login, background='orange', foreground='white', font=("Arial", 15, "bold")).place(x=140, y=130)
 janela.mainloop()
