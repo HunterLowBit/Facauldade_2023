f88784fd28e9a7bd943d45685006b944f7e507e5
Robson <53387930+HunterLowBit@users.noreply.github.com>
2023-04-15 19:13:32 -0300

    Update tk.py

--- Interface_TK/Aula_7.1-menu_scroll/tk.py
+++ Interface_TK/Aula_7.1-menu_scroll/tk.py
@@ -1,10 +1,10 @@
 from tkinter import *
 from tkinter import messagebox
 janela = Tk()
-janela.title("-")
+janela.title("TESTE DE TEXTO")
 janela.geometry("300x200")
 janela.config(background='black')
-janela.option_add('*tearOff', FALSE)
+janela.option_add('tearOff', FALSE)
 
 
 def mensagem():
@@ -19,7 +19,6 @@ filemenu.add_separator()
 filemenu.add_command(label="Sair", command=janela.quit) 
 
 menubar.add_command(label="Sair", command=janela.quit)
-label = Label(text="Primeiramente", bg='black', fg='white', font=('times new roman', 20,'bold')).place(x=100,y=100)  
 
 texto = Text(janela)
 texto.grid(row=0, column=0, sticky="nsew")
