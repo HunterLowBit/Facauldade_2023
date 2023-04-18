668f85ae49c703702803268d0fe49a98d3d704ea
Robson <53387930+HunterLowBit@users.noreply.github.com>
2023-04-18 00:42:49 -0300

    update

--- Interface_TK/Aula_8.1-Cadastro de itens/db.py
+++ Interface_TK/Aula_8.1-Cadastro de itens/db.py
@@ -1,8 +1,11 @@
 import sqlite3
+
+
 def cria_db(produto, preco):
     conn = sqlite3.connect('produtos.db')
     bd = conn.cursor()
     bd.execute('CREATE TABLE IF NOT EXISTS Cadastro (id INTEGER PRIMARY KEY AUTOINCREMENT, produto VARCHAR(100), preço REAL NOT NULL, descricao VARCHAR(200), fk_categoria INTEGER)')
-    bd.execute("INSERT INTO produtos (produto, preço) VALUES (?,?)",(produto,preco))
+    bd.execute("INSERT INTO produtos (produto, preço) VALUES (?,?)",
+               (produto, preco))
     conn.commit()
     conn.close()
