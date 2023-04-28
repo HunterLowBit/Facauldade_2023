d2102c4cf87eeae85bf7451e2ee77051f749d886
Robson <53387930+HunterLowBit@users.noreply.github.com>
2023-04-27 23:56:27 -0300

    Update testes.py

--- Interface_TK/Aula_9.1-folha_pagamento_tk/testes.py
+++ Interface_TK/Aula_9.1-folha_pagamento_tk/testes.py
@@ -3,6 +3,7 @@ import tkinter.messagebox as msg_box
 # Função para calcular o desconto
 def calcular_desconto():
     salario = float(entrada_salario.get())
+    nome = str(entrada_nome.get())
 
     if salario <= 1903.98:
         desconto_ir = 0
@@ -15,17 +16,29 @@ def calcular_desconto():
     else:
         desconto_ir = (salario * 0.275) - 869.36
         
+    desconto_alimentacao = salario * 0.01
+
+    vale_transporte = salario * 0.06
+       
     desconto_inss = salario * 0.11
-    salario_liquido = salario - desconto_ir - desconto_inss
+    salario_liquido = salario - desconto_ir - desconto_inss - desconto_alimentacao
+    
+    n_liquido = salario_liquido - vale_transporte
 
     msg_box.showinfo(title="Desconto de Folha de Pagamento", 
-                     message=f"""Salário: R${salario:.2f}
-Desconto IR: R${desconto_ir:.2f}
-Desconto INSS: R${desconto_inss:.2f}
-                     
-Salário Líquido: R${salario_liquido:.2f}""")
+                     message=f"""
+Nome: {nome}          
+Salário: R$ {salario:.2f}
+__________________________________
+Desconto IR: R$ {desconto_ir:.2f}
+Desconto INSS: R$ {desconto_inss:.2f}
+Desconto Vale-Refeição: R$ {desconto_alimentacao:.2f}
+___________________________________
+Salário Líquido: R$ {salario_liquido:.2f}
+Vale-Transporte: R$ {vale_transporte:.2f}
+__________________________________
+Salário Líquido Final: R$ {n_liquido:.2f}""")
 
-    
 # Cria a janela principal
 janela = tk.Tk()
 
@@ -36,9 +49,6 @@ entrada_nome = tk.Entry(janela)
 rotulo_salario = tk.Label(janela, text="Salario:")
 entrada_salario = tk.Entry(janela)
 
-rotulo_cargo = tk.Label(janela, text="Cargo:")
-entrada_cargo = tk.Entry(janela)
-
 # Menubar
 menu_bar = tk.Menu(janela)
 
@@ -46,12 +56,11 @@ calcular_menu = tk.Menu(menu_bar, tearoff=0)
 menu_bar.add_cascade(label="Calcular Desconto", menu=calcular_menu)
 calcular_menu.add_command(label="Calcular", command=calcular_desconto)
 
-
 janela.config(menu=menu_bar)
 
 # Define as propriedades dos widgets
 janela.title("Cadastro de Clientes")
-janela.geometry("400x150")
+janela.geometry("300x100")
 
 # Adiciona os widgets à janela
 rotulo_nome.pack()
@@ -59,9 +68,6 @@ entrada_nome.pack()
 
 rotulo_salario.pack()
 entrada_salario.pack()
-
-rotulo_cargo.pack()
-entrada_cargo.pack()
 
 # Inicia o loop principal da janela
 janela.mainloop()
