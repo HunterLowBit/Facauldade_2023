6bb0043050305f7f7dd14fe422c2b6caa5ad771a
Robson <53387930+HunterLowBit@users.noreply.github.com>
2023-06-14 02:42:08 -0300

    Falta terminar as funções

---
+++ Testes/aleatorio/Busca_Letra.py
@@ -0,0 +1,5 @@
+d_visibilidade = "1 asda"
+distancia_visibilidade = d_visibilidade[2] + d_visibilidade[4]
+print(distancia_visibilidade)
+# Ta buscando dentro da varialvel a letra "A", Posição 3 (2)
+# Somando letras
